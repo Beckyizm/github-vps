8b1cb6e81531d6be14778e539919bd450d768b44
Beckyizm <136853955+Beckyizm@users.noreply.github.com>
2023-11-03 17:44:14 +0300

    Update linux_configure_rdp.sh
    Test

--- scripts/linux_configure_rdp.sh
+++ scripts/linux_configure_rdp.sh
@@ -6,8 +6,8 @@ sudo apt update
 sudo apt install -y xfce4 xfce4-goodies xfonts-base xubuntu-icon-theme xubuntu-wallpapers gnome-icon-theme x11-apps x11-common x11-session-utils x11-utils x11-xserver-utils x11-xkb-utils dbus-user-session dbus-x11 gnome-system-monitor gnome-control-center libpam0g libxt6 libxext6
 
 # Second, install TurboVNC
-#wget https://phoenixnap.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc_2.2.5_amd64.deb
-#sudo dpkg -i turbovnc_2.2.5_amd64.deb
+wget https://phoenixnap.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc_2.2.5_amd64.deb
+sudo dpkg -i turbovnc_2.2.5_amd64.deb
 
 sudo apt install ubuntu-desktop xrdp -y
 sudo adduser xrdp ssl-cert
@@ -31,9 +31,9 @@ sudo ufw reload
 
 
 # Third, download ngrok
-# wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-# unzip ngrok-stable-linux-amd64.zip
-# chmod +x ngrok
+ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+ unzip ngrok-stable-linux-amd64.zip
+ chmod +x ngrok
 
 # Fourth, generate and copy passwd file and xstartup script
 #export PATH=$PATH:/opt/TurboVNC/bin
